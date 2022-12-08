45443782cf34f24eb474ddf7bfdd9487eb66cab8
kasper <kasperfriangt@gmail.com>
2022-12-08 02:45:55 +0100

    added coach pages

---
+++ ScSoMeBlazorServer/ScSoMeBlazorServer/Data/CoachService/ICoachService.cs
@@ -0,0 +1,13 @@
+﻿using ScSoMeBlazorServer.Models;
+
+namespace ScSoMeBlazorServer.Data.CoachService
+{
+    public interface ICoachService
+    {
+        public Task<List<Coach>> GetCoaches();
+
+        public Task<Coach> GetCoach(string coach);
+
+        public Task PostCoach(Coach coach);
+    }
+}
