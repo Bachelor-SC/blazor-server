2a3354c2539aa722e4838c357669640259e169f4
kasper <kasperfriangt@gmail.com>
2022-12-13 16:48:57 +0100

    added blocking functionality

--- ScSoMeBlazorServer/ScSoMeBlazorServer/Data/ConnectionService/ConnectionService.cs
+++ ScSoMeBlazorServer/ScSoMeBlazorServer/Data/ConnectionService/ConnectionService.cs
@@ -89,6 +89,87 @@ namespace ScSoMeBlazorServer.Data.ConnectionService
 
         }
 
+        public async Task DeleteConnection(string username, string connectionUsername)
+        {
+            await client.deleteFromAPI($"Connections/DeleteConnection?user={username}&connectionUsername={connectionUsername}");
+        }
+
+        // BLOCKS SECTION
+        public async Task AddBlock(string username, string connectionUsername)
+        {
+            Connection con = new Connection
+            {
+                usernameCon1 = username,
+                usernameCon2 = connectionUsername,
+                createdDate = DateTime.Now
+            };
+
+            await client.postToAPI($"Connections/AddBlock", con);
+            await logBlockActivity(username, connectionUsername,"blocked");
+            
+
+        }
+        public async Task<List<Connection>> GetBlocksForUser(string username)
+        {
+            string json = await client.getFromAPI($"Connections/BlocksForUser?user={username}");
+
+            return createList(json, username);
+
+            static List<Connection> createList(string json, string user)
+            {
+                try
+                {
+                    List<Connection>? list = JsonSerializer.Deserialize<List<Connection>>(json);
+                    return list ?? new List<Connection> {new Connection{
+                    usernameCon1 = "Empty",
+                    createdDate = DateTime.Now,
+                    usernameCon2 = "List returned empty"
+
+
+                }
+                };
+                }
+                catch (Exception ex)
+                {
+                    List<Connection> list = new List<Connection>();
+                    list.Add(new Connection
+                    {
+                        usernameCon1 = "Error",
+                        createdDate = DateTime.Now,
+                        usernameCon2 = ex.Message
+
+                    });
+                    return list;
+
+                }
+            }
+        }
+
+        public async Task<string> CheckIfBlocked(string username, string connectionUsername)
+        {
+            string json = await client.getFromAPI($"Connections/CheckIfBlocked?user={username}&connectionUsername={connectionUsername}");
+            return json;
+
+        }
+        public async Task DeleteBlock(string username, string connectionUsername)
+        {
+            await client.deleteFromAPI($"Connections/DeleteBlock?user={username}&connectionUsername={connectionUsername}");
+            await logBlockActivity(username, connectionUsername,"unblocked");
+        }
+
+        private async Task logBlockActivity(string activityTargetusername, string blockedUsername,string action)
+        {
+            await activityService.LogActivity(new Activity
+            {
+                type = "Block",
+                action = "add",
+                date = DateTime.Now,
+                additionalInfo = $"The user {activityTargetusername} {action} {blockedUsername}",
+                username = activityTargetusername
+
+            }, activityTargetusername);
+        }
+
 
     }
 }
